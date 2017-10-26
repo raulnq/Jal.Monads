c71c7ba22f2cdb9d868a0e3e03960783fe612632
raul naupari <raul.naupari@cignium.com>
2017-10-25 19:56:20 -0500

    foreach

---
+++ Jal.Monads/EnumerableExtensions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace Jal.Monads
+{
+    public static class EnumerableExtensions
+    {
+        public static Result ForEach<TElement>(this IEnumerable<TElement> array, Func<TElement, Result> itemfunc)
+        {
+            foreach (var element in array)
+            {
+                var result = itemfunc(element);
+
+                if (result.IsFailure)
+                {
+                    return result;
+                }
+            }
+
+            return Result.Success();
+        }
+
+        public static Result<TContent> ForEach<TElement, TContent>(this IEnumerable<TElement> array, Func<TElement, Result> itemfunc, Func<TContent> endfunc)
+        {
+            foreach (var element in array)
+            {
+                var result = itemfunc(element);
+
+                if (result.IsFailure)
+                {
+                    return Result.Failure<TContent>(result.Errors);
+                }
+            }
+
+            return Result.Success<TContent>(endfunc());
+        }
+    }
+}
