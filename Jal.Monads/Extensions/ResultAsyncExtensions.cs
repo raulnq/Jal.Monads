5689f42e6b4ce50003736602b4bd10da1df49307
raulnq <raulnq@gmail.com>
2019-08-11 12:08:45 -0500

    more async methods

--- Jal.Monads/Extensions/ResultAsyncExtensions.cs
+++ Jal.Monads/Extensions/ResultAsyncExtensions.cs
@@ -44,9 +44,133 @@ namespace Jal.Monads.Extensions
             return ResultCoreAsyncExtensions.BindAsync(result, onsuccess);
         }
 
+        public static Task<Result<T, E>> OnBothAsync<T, E>(this Task<Result<T, E>> result, Action<T> onsuccess, Action<E> onfailure)
+        {
+            return MonitorAsync(result, onsuccess, onfailure);
+        }
+
+        public static Task<Result<E>> OnBothAsync<E>(this Task<Result<E>> result, Action onsuccess, Action<E> onfailure)
+        {
+            return MonitorAsync(result, onsuccess, onfailure);
+        }
+
+        public static Task<Result<T, E>> OnBothAsync<T, E>(this Task<Result<T, E>> result, Func<T, Task> onsuccess, Func<E, Task> onfailure)
+        {
+            return MonitorAsync(result, onsuccess, onfailure);
+        }
+
+        public static Task<Result<E>> OnBothAsync<E>(this Task<Result<E>> result, Func<Task> onsuccess, Func<E, Task> onfailure)
+        {
+            return MonitorAsync(result, onsuccess, onfailure);
+        }
+
         public static Task<Result<E>> OnSuccessAsync<T, E>(this Task<Result<T, E>> result, Func<T, Task<Result<E>>> onsuccess)
         {
             return ResultCoreAsyncExtensions.BindAsync(result, onsuccess);
+        }
+
+        public static Task<Result<T, E>> OnFailureAsync<T, E>(this Task<Result<T, E>> result, Action<E> action)
+        {
+            return MonitorAsync(result, onfailure: action);
+        }
+
+        public static Task<Result<E>> OnFailureAsync<E>(this Task<Result<E>> result, Action<E> action)
+        {
+            return MonitorAsync(result, onfailure: action);
+        }
+
+        public static Task<Result<T, E>> OnFailureAsync<T, E>(this Task<Result<T, E>> result, Func<E, Task> action)
+        {
+            return MonitorAsync(result, onfailure: action);
+        }
+
+        public static Task<Result<E>> OnFailureAsync<E>(this Task<Result<E>> result, Func<E, Task> action)
+        {
+            return MonitorAsync(result, onfailure: action);
+        }
+
+        public static Task<Result<T, E>> OnSuccessAsync<T, E>(this Task<Result<T, E>> result, Action<T> onsuccess)
+        {
+            return MonitorAsync(result, onsuccess: onsuccess);
+        }
+
+        public static Task<Result<T, E>> OnSuccessAsync<T, E>(this Task<Result<T, E>> result, Func<T, Task> onsuccess)
+        {
+            return MonitorAsync(result, onsuccess: onsuccess);
+        }
+
+        public static Task<Result<E>> OnSuccessAsync<E>(this Task<Result<E>> result, Action onsuccess)
+        {
+            return MonitorAsync(result, onsuccess: onsuccess);
+        }
+
+        public static Task<Result<E>> OnSuccessAsync<E>(this Task<Result<E>> result, Func<Task> onsuccess)
+        {
+            return MonitorAsync(result, onsuccess: onsuccess);
+        }
+
+        public static async Task<Result<T, E>> MonitorAsync<T, E>(this Task<Result<T, E>> result, Action<T> onsuccess = null, Action<E> onfailure = null)
+        {
+            var r = await result;
+
+            if (r.IsSuccess)
+            {
+                onsuccess?.Invoke(r.Content);
+            }
+            else
+            {
+                onfailure?.Invoke(r.Error);
+            }
+
+            return r;
+        }
+
+        public static async Task<Result<E>> MonitorAsync<E>(this Task<Result<E>> result, Action onsuccess = null, Action<E> onfailure = null)
+        {
+            var r = await result;
+
+            if (r.IsSuccess)
+            {
+                onsuccess?.Invoke();
+            }
+            else
+            {
+                onfailure?.Invoke(r.Error);
+            }
+
+            return r;
+        }
+
+        public static async Task<Result<T, E>> MonitorAsync<T, E>(this Task<Result<T, E>> result, Func<T, Task> onsuccess = null, Func<E, Task> onfailure = null)
+        {
+            var r = await result;
+
+            if (r.IsSuccess)
+            {
+                await onsuccess?.Invoke(r.Content);
+            }
+            else
+            {
+                await onfailure?.Invoke(r.Error);
+            }
+
+            return r;
+        }
+
+        public static async Task<Result<E>> MonitorAsync<E>(this Task<Result<E>> result, Func<Task> onsuccess = null, Func<E, Task> onfailure = null)
+        {
+            var r = await result;
+
+            if (r.IsSuccess)
+            {
+                await onsuccess?.Invoke();
+            }
+            else
+            {
+                await onfailure?.Invoke(r.Error);
+            }
+
+            return r;
         }
     }
 }
